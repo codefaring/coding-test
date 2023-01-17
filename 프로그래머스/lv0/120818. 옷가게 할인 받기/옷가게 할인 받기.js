b8d8132963f562f5b7e5a5b3b54938890b5cb88f
codefaring <121625285+codefaring@users.noreply.github.com>
2023-01-17 23:36:37 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,10 @@
+function solution(price) {
+    if(500000 <= price) {
+        return Math.floor(price - price * 0.2);
+    } else if(300000 <= price) {
+        return Math.floor(price - price * 0.1);
+    } else if(100000 <= price) {
+        return Math.floor(price - price * 0.05);
+    }
+    return price;
+}
